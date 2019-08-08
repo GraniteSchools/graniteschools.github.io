9a7398e454885d3902040859f8a682b77c3ca2e3
GraniteSchools <8070_GraniteSchools@users.noreply.github.com>
2019-08-08 14:16:59 -0600

    Update BusSearch_js_328api.js
    comma

--- BusSearch_js_328api.js
+++ BusSearch_js_328api.js
@@ -407,7 +407,7 @@ BS_Sr.setInfoTemplates({0: {infoTemplate: busTemplate}});
 		overLyr = new WebTiledLayer('https://discover.agrc.utah.gov/login/path/fuel-cola-scoop-canyon/tiles/overlay_basemap/${level}/${col}/${row}',{minScale: 8000});
 		map.addLayers([terLyr,imgLyr,overLyr]);
 
-    map.addLayers([ElPoly,JrPoly,SrPoly,busElem,busJr,busSrBS_El,BS_Jr,BS_Sr]);
+    map.addLayers([ElPoly,JrPoly,SrPoly,busElem,busJr,busSr,BS_El,BS_Jr,BS_Sr]);
 
     map.infoWindow.resize(250,75);
 
